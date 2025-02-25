a156b4fc46c2c537b66e12cb55bcf8dc6f2583a0
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-02-25 17:19:54 +0800

    feat: add start menu

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import pygame
 import sys
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from player import Player
 from shot import Shot
-import pygame
 from constants import *
 
 
@@ -16,41 +16,67 @@ def main():
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
 
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     Shot.containers = (updatable, drawable, shots)
+
+    
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
     asteriod_field = AsteroidField()
 
+    menu_title = pygame.font.Font("font/Pixeltype.ttf", 150).render("Asteroid", False, "White").convert()
+    menu_title_rect = menu_title.get_rect(center = (x, y - 130))
+
+    menu_message = pygame.font.Font("font/Pixeltype.ttf", 50).render("Press SPACE to start", False, "White").convert()
+    menu_message_rect = menu_message.get_rect(center = (x, y + 120))
+
+    game_active = False
     dt = 0
 
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        updatable.update(dt)
+            if game_active:
+                pass
 
-        for asteroid in asteroids:
-            if asteroid.collision(player):
-                print("Game over!")
-                sys.exit()
+            else:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        game_active = True
 
-            for shot in shots:
-                if asteroid.collision(shot):
-                    asteroid.split()
-                    shot.kill()
 
-            
+        if game_active:
+            updatable.update(dt)
 
-        screen.fill("black")
+            for asteroid in asteroids:
+                if asteroid.collision(player):
+                    print("Game over!")
+                    game_active = False
 
-        for item in drawable:
-            item.draw(screen)
+                for shot in shots:
+                    if asteroid.collision(shot):
+                        asteroid.split()
+                        shot.kill()
+
+                
+
+            screen.fill("black")
+
+            for item in drawable:
+                item.draw(screen)
+
+        else:
+            screen.blit(menu_title, menu_title_rect)
+            screen.blit(menu_message, menu_message_rect)
+
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
