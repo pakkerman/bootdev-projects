b2812a1fc04f1bb56790564c0b6ca469b6662d66
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-03-05 15:36:34 +0800

    feat: add player side boosters to q and e keys

--- player.py
+++ player.py
@@ -8,9 +8,12 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.radius = PLAYER_RADIUS
         self.rotation = 0
-        self.shoot_timer = 0
+        self.shoot_cooldown = 0
         self.inertia = pygame.Vector2(0, 0)
         self.acceleration = pygame.Vector2(0, 0.4)
+
+        self.boost_cooldown = 0
+        self.boost_acceleration = self.acceleration * 10
 
 
     def triangle(self):
@@ -34,17 +37,31 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
-        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
+        # front and back movement
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+        # side boosters
+        if keys[pygame.K_q]:
+            if self.boost_cooldown <= 0:
+                self.inertia += self.boost_acceleration.rotate(self.rotation - 90)
+                self.boost_cooldown = 1.5
+        if keys[pygame.K_e]:
+            if self.boost_cooldown <= 0:
+                self.inertia += self.boost_acceleration.rotate(self.rotation + 90)
+                self.boost_cooldown = 1.5
+
+        # rotation
         if keys[pygame.K_w]: 
             self.inertia += self.acceleration.rotate(self.rotation)
         if keys[pygame.K_s]:
             self.inertia -= self.acceleration.rotate(self.rotation)
+        
+        # fire weapon
         if keys[pygame.K_SPACE]:
             self.shoot()
         
@@ -54,15 +71,20 @@ class Player(CircleShape):
         self.position.x %= SCREEN_WIDTH
         self.position.y %= SCREEN_HEIGHT
             
+        # cooldown timers
+        self.shoot_cooldown -= dt
+        if 0 < self.boost_cooldown:
+            self.boost_cooldown -= dt
+        
 
 
     def shoot(self):
-        if 0 < self.shoot_timer:
+        if 0 < self.shoot_cooldown:
             return
 
         shot = Shot(self.position.x, self.position.y) 
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED  
         self.inertia -= pygame.Vector2(0, 0.3).rotate(self.rotation) # firing recoil
 
-        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+        self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN
 
