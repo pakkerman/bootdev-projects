86eebd245369064df05a458c1e787db42b9442d4
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-03-05 17:00:33 +0800

    fix: invisible asteroid

--- asteroid.py
+++ asteroid.py
@@ -3,16 +3,17 @@ import random
 from circleshape import CircleShape
 from constants import *
 
-
-
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        
+        self.color = f"#cccccc"
+
 
     def split(self):
         self.kill()
 
-        if self.radius < ASTEROID_MIN_RADIUS:
+        if self.radius <= ASTEROID_MIN_RADIUS:
             return
         
         deg = random.uniform(20, 50)
@@ -26,10 +27,14 @@ class Asteroid(CircleShape):
 
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+        pygame.draw.circle(screen, self.color, self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+
+
+
 
 
 
